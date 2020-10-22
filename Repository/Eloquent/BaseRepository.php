ed8401816eef5ae99d8258e0c361760d4f14db4d
殷志军 <2353171512@qq.com>
2020-10-22 15:01:15 +0800

    分离出仓库自定义查询方法

--- Repository/Eloquent/BaseRepository.php
+++ Repository/Eloquent/BaseRepository.php
@@ -4,9 +4,7 @@ namespace Lyndon\Repository\Eloquent;
 
 use Lyndon\Model\BaseModel;
 use Illuminate\Container\Container;
-use Illuminate\Pagination\Paginator;
 use Lyndon\Exceptions\RepositoryException;
-use Illuminate\Pagination\LengthAwarePaginator;
 use Lyndon\Repository\Contracts\CriteriaInterface;
 use Lyndon\Repository\Contracts\RepositoryInterface;
 use Lyndon\Repository\Contracts\RepositoryCriteriaInterface;
@@ -19,6 +17,8 @@ use Lyndon\Repository\Contracts\RepositoryCriteriaInterface;
  */
 abstract class BaseRepository implements RepositoryInterface, RepositoryCriteriaInterface
 {
+    use RepositoryCommonMethodsTrait;
+
     /**
      * @var BaseModel
      */
@@ -56,13 +56,6 @@ abstract class BaseRepository implements RepositoryInterface, RepositoryCriteria
      * @var array
      */
     protected $defaultOrderByFields = [];
-
-    /**
-     * 全局范围查询
-     *
-     * @var array
-     */
-    protected $scopeQuery = [];
 
     /**
      * array of Criteria
@@ -275,163 +268,5 @@ abstract class BaseRepository implements RepositoryInterface, RepositoryCriteria
         }
 
         return $this->model = $model;
-    }
-
-    /**
-     * 追加全局范围查询
-     *
-     * @param $scopeQuery
-     */
-    public function pushScopeQuery($scopeQuery)
-    {
-        $this->scopeQuery = array_merge($this->scopeQuery, $scopeQuery);
-    }
-
-    /**
-     * 当前查询应用范围
-     *
-     * @return $this
-     */
-    public function applyScopeQuery()
-    {
-        if (! empty($this->scopeQuery)) {
-            $this->model = $this->model->where($this->scopeQuery);
-        }
-
-        return $this;
-    }
-
-    /**
-     * 添加数据行
-     *
-     * @param array $param
-     * @return mixed
-     * @throws \Lyndon\Exceptions\ModelException
-     */
-    public function addRepoRow(array $param)
-    {
-        return $this->model->addRow($param);
-    }
-
-    /**
-     * 批量添加数据列
-     *
-     * @param array $param
-     * @return mixed
-     */
-    public function batchAddRepoList(array $param)
-    {
-        return $this->model->insert($param);
-    }
-
-    /**
-     * 编辑数据行
-     *
-     * @param mixed $primaryKey
-     * @param array $param
-     * @param array $extraWhere
-     * @return mixed
-     * @throws \Lyndon\Exceptions\ModelException
-     */
-    public function editRepoRow($primaryKey, array $param, array $extraWhere = [])
-    {
-        $extraWhere = array_merge($this->scopeQuery, $extraWhere);
-
-        return $this->model->editRow($primaryKey, $param, $extraWhere);
-    }
-
-    /**
-     * 获取一行数据
-     *
-     * @param mixed $primaryKey
-     * @param array $extraWhere
-     * @param string $trashed -- onlyTrashed / withTrashed
-     * @return array
-     */
-    public function getRepoRowByPrimaryKey($primaryKey, array $extraWhere = [], $trashed = '')
-    {
-        $extraWhere = array_merge($this->scopeQuery, $extraWhere);
-
-        return $this->model->getRowByPrimaryKey($primaryKey, $extraWhere, $trashed);
-    }
-
-    /**
-     * 验证一行数据存在性
-     *
-     * @param mixed $primaryKey
-     * @param array $extraWhere
-     * @param string $trashed -- onlyTrashed / withTrashed
-     * @return bool
-     */
-    public function existsRepoRowByPrimaryKey($primaryKey, array $extraWhere = [], $trashed = '')
-    {
-        $extraWhere = array_merge($this->scopeQuery, $extraWhere);
-
-        return $this->model->existsRowByPrimaryKey($primaryKey, $extraWhere, $trashed);
-    }
-
-    /**
-     * 获取多行数据
-     *
-     * @param array $primaryKeys
-     * @param array $extraWhere
-     * @param string $trashed -- onlyTrashed / withTrashed
-     * @return array
-     */
-    public function getRepoListByPrimaryKeys($primaryKeys, $extraWhere = [], $trashed = '')
-    {
-        $extraWhere = array_merge($this->scopeQuery, $extraWhere);
-
-        return $this->model->getListByPrimaryKeys($primaryKeys, $extraWhere, $trashed);
-    }
-
-    /**
-     * 删除多条数据
-     *
-     * @param array $primaryKeys
-     * @param array $extraWhere
-     * @param string $deleteMethod -- delete / forceDelete
-     * @return bool
-     */
-    public function destroyRepoByPrimaryKeys($primaryKeys, array $extraWhere = [], $deleteMethod = 'delete')
-    {
-        $extraWhere = array_merge($this->scopeQuery, $extraWhere);
-
-        return $this->model->destroyByPrimaryKeys($primaryKeys, $extraWhere, $deleteMethod);
-    }
-
-    /**
-     * 自定义分页，在获取所有主键ID之后进行的分页
-     *
-     * @param array $allDataList
-     * @param $pageSize
-     * @return array
-     */
-    public function selfPaginate($allDataList, $pageSize)
-    {
-        // 数据总条数
-        $total = count($allDataList);
-
-        // 当前页数
-        $currentPage = request()->get('page');
-        $currentPage = empty($currentPage) ? 1 : $currentPage;
-
-        // 每页数据条数
-        $pageSize = (empty($pageSize) || intval($pageSize) < 1) ? 15 : intval($pageSize);
-
-        // 当前页数据列表
-        $items = array_slice($allDataList, ($currentPage - 1) * $pageSize, $pageSize);
-
-        // 获取分页数据
-        $options = [
-            'path' => Paginator::resolveCurrentPath(),
-            'pageName' => 'page'
-        ];
-        $result = new LengthAwarePaginator(
-            $items, $total, $pageSize, $currentPage, $options
-        );
-        $result = $result->appends(request()->input())->toArray();
-
-        return $result;
     }
 }
---
+++ Repository/Eloquent/RepositoryCommonMethodsTrait.php
@@ -0,0 +1,181 @@
+<?php
+namespace Lyndon\Repository\Eloquent;
+
+
+use Lyndon\Model\BaseModel;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+/**
+ * Trait RepositoryCommonMethodsTrait
+ * @package Lyndon\Repository\Eloquent
+ *
+ * @property BaseModel $model
+ */
+trait RepositoryCommonMethodsTrait
+{
+    /**
+     * 全局范围查询
+     *
+     * @var array
+     */
+    protected $scopeQuery = [];
+
+    /**
+     * 追加全局范围查询
+     *
+     * @param $scopeQuery
+     */
+    public function pushScopeQuery($scopeQuery)
+    {
+        $this->scopeQuery = array_merge($this->scopeQuery, $scopeQuery);
+    }
+
+    /**
+     * 当前查询应用范围
+     *
+     * @return $this
+     */
+    public function applyScopeQuery()
+    {
+        if (! empty($this->scopeQuery)) {
+            $this->model = $this->model->where($this->scopeQuery);
+        }
+
+        return $this;
+    }
+
+    /**
+     * 添加数据行
+     *
+     * @param array $param
+     * @return mixed
+     * @throws \Lyndon\Exceptions\ModelException
+     */
+    public function addRepoRow(array $param)
+    {
+        return $this->model->addRow($param);
+    }
+
+    /**
+     * 批量添加数据列
+     *
+     * @param array $param
+     * @return mixed
+     */
+    public function batchAddRepoList(array $param)
+    {
+        return $this->model->insert($param);
+    }
+
+    /**
+     * 编辑数据行
+     *
+     * @param mixed $primaryKey
+     * @param array $param
+     * @param array $extraWhere
+     * @return mixed
+     * @throws \Lyndon\Exceptions\ModelException
+     */
+    public function editRepoRow($primaryKey, array $param, array $extraWhere = [])
+    {
+        $extraWhere = array_merge($this->scopeQuery, $extraWhere);
+
+        return $this->model->editRow($primaryKey, $param, $extraWhere);
+    }
+
+    /**
+     * 获取一行数据
+     *
+     * @param mixed $primaryKey
+     * @param array $extraWhere
+     * @param string $trashed -- onlyTrashed / withTrashed
+     * @return array
+     */
+    public function getRepoRowByPrimaryKey($primaryKey, array $extraWhere = [], $trashed = '')
+    {
+        $extraWhere = array_merge($this->scopeQuery, $extraWhere);
+
+        return $this->model->getRowByPrimaryKey($primaryKey, $extraWhere, $trashed);
+    }
+
+    /**
+     * 验证一行数据存在性
+     *
+     * @param mixed $primaryKey
+     * @param array $extraWhere
+     * @param string $trashed -- onlyTrashed / withTrashed
+     * @return bool
+     */
+    public function existsRepoRowByPrimaryKey($primaryKey, array $extraWhere = [], $trashed = '')
+    {
+        $extraWhere = array_merge($this->scopeQuery, $extraWhere);
+
+        return $this->model->existsRowByPrimaryKey($primaryKey, $extraWhere, $trashed);
+    }
+
+    /**
+     * 获取多行数据
+     *
+     * @param array $primaryKeys
+     * @param array $extraWhere
+     * @param string $trashed -- onlyTrashed / withTrashed
+     * @return array
+     */
+    public function getRepoListByPrimaryKeys($primaryKeys, $extraWhere = [], $trashed = '')
+    {
+        $extraWhere = array_merge($this->scopeQuery, $extraWhere);
+
+        return $this->model->getListByPrimaryKeys($primaryKeys, $extraWhere, $trashed);
+    }
+
+    /**
+     * 删除多条数据
+     *
+     * @param array $primaryKeys
+     * @param array $extraWhere
+     * @param string $deleteMethod -- delete / forceDelete
+     * @return bool
+     */
+    public function destroyRepoByPrimaryKeys($primaryKeys, array $extraWhere = [], $deleteMethod = 'delete')
+    {
+        $extraWhere = array_merge($this->scopeQuery, $extraWhere);
+
+        return $this->model->destroyByPrimaryKeys($primaryKeys, $extraWhere, $deleteMethod);
+    }
+
+    /**
+     * 自定义分页，在获取所有主键ID之后进行的分页
+     *
+     * @param array $allDataList
+     * @param $pageSize
+     * @return array
+     */
+    public function selfPaginate($allDataList, $pageSize)
+    {
+        // 数据总条数
+        $total = count($allDataList);
+
+        // 当前页数
+        $currentPage = request()->get('page');
+        $currentPage = empty($currentPage) ? 1 : $currentPage;
+
+        // 每页数据条数
+        $pageSize = (empty($pageSize) || intval($pageSize) < 1) ? 15 : intval($pageSize);
+
+        // 当前页数据列表
+        $items = array_slice($allDataList, ($currentPage - 1) * $pageSize, $pageSize);
+
+        // 获取分页数据
+        $options = [
+            'path' => Paginator::resolveCurrentPath(),
+            'pageName' => 'page'
+        ];
+        $result = new LengthAwarePaginator(
+            $items, $total, $pageSize, $currentPage, $options
+        );
+        $result = $result->appends(request()->input())->toArray();
+
+        return $result;
+    }
+}
